6c9f9fe66af544fdcb023ad5aadd29aaa10e4108
onchari <onchariwycliffe@gmail.com>
2018-06-20 17:08:24 +0300

    Add project files.

---
+++ OneStopElectronix/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OneStopElectronix.Startup))]
+namespace OneStopElectronix
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
