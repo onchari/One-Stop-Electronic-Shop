a5a3284faacf64773ec0ab972db0b909b5fecf3b
onchari <onchariwycliffe@gmail.com>
2018-06-20 18:46:07 +0300

    First

---
+++ OneStopElectronix/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OneStopElectronix.Areas.Admin.Controllers
+{
+    [RouteArea("Admin")]
+    [Route("admin/[controller]")]
+    public class AdminController : Controller
+    {
+        // GET: Admin/Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
